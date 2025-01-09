a6779f57a89f4ac11f5500455beb0cff225f5134
Luiz Felipe Rodrigues de Melo <mail.lfellipe@gmail.com>
2025-01-09 10:57:37 -0300

    commit 09/01

--- website-lfellipestudios-app/src/App.jsx
+++ website-lfellipestudios-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomebarComponent from './components/Homebar/HomebarComponent'
 import StartComponent from './components/Start/StartComponent'
 import SobreComponent from './components/Sobre/SobreComponent'
@@ -6,14 +6,15 @@ import PortifolioComponent from './components/Portifolio/PortifolioComponent'
 import TituloComponente from './components/Titulos/TituloComponente'
 
 const App = () => {
+
   return (
     <div>
       <HomebarComponent />
       <StartComponent />
       <SobreComponent/>
-      <div className="container portifolio-container">
-        <TituloComponente titulo="Portifólio" texto="Descubra meus mais diversos projetos." />
-      <PortifolioComponent />
+        <div className="container portifolio-container">
+          <TituloComponente titulo="Portifólio" texto="Descubra meus projetos mais recentes" />
+          <PortifolioComponent />
       </div>
     </div>
   )
