910e0d084b5b1a55b28e59f83953a78c377bba27
Luiz Felipe Rodrigues de Melo <mail.lfellipe@gmail.com>
2025-03-01 18:44:19 -0300

    commit 01/03 v.1.5.4
    mudanças no start

--- website-lfellipestudios-app/src/components/Start/StartComponent.jsx
+++ website-lfellipestudios-app/src/components/Start/StartComponent.jsx
@@ -8,7 +8,7 @@ const StartComponent = () => {
       <div className="start-text">
         <h1>Luiz Felipe</h1>
         <p>Design & Front-End</p>
-        <button className='default-button'><Link to= "portifolio" smooth={true} offset={-190} duration={500}><h1>Explorar Conteúdo</h1></Link></button>
+        <button className='default-button'><Link to= "portifolio" smooth={true} offset={-190} duration={500}><h1>Descobrir Conteúdo</h1></Link></button>
       </div>
     </main>
   )
